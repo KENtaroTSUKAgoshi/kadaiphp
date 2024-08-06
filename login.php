fbb0c8e7b5febe9e4ea86a9afc937466e6fdfb48
KENtaroTSUKAgoshi <168268428+KENtaroTSUKAgoshi@users.noreply.github.com>
2024-08-06 15:21:53 +0900

    PHP

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/login.css" />
+    <link rel="stylesheet" href="css/common.css" />
+    <title>会員ログイン</title>
+</head>
+
+<body>
+
+    <header>
+        <nav class="navbar navbar-default">会員LOGIN</nav>
+    </header>
+    <form name="form1" action="login_act.php" method="post">
+        ID:<input type="text" name="lid" />
+        PW:<input type="password" name="lpw" />
+        <input type="submit" value="LOGIN" />
+    </form>
+</body>
+
+</html>
